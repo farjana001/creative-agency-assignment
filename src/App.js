b5bf507a0ebb5e9858accbbc41df6fe259ff98ad
Farjana Islam <nipa.farzana87@gmailcon>
2020-10-15 22:21:14 +0600

    fixing private route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createContext } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Components/Home/Home/Home'
+import Login from './Components/Pages/Login/Login/Login';
+import ServiceList from './Components/Pages/Customer/ServiceList/ServiceList';
+import Review from './Components/Pages/Customer/Review/Review';
+import AdminServiceList from './Components/Pages/Admin/AdminServiceList/AdminServiceList';
+import MakeAdmin from './Components/Pages/Admin/MakeAdmin/MakeAdmin';
+import Order from './Components/Pages/Customer/Order/Order';
+import { useState } from 'react';
+import service1 from './images/icons/service1.png';
+import service2 from './images/icons/service2.png';
+import service3 from './images/icons/service3.png';
+import AdminHome from './Components/Pages/Admin/AdminHome/AdminHome';
+import Admin from './Components/Pages/Admin/Admin/Admin';
+import { useEffect } from 'react';
+import NoMatch from './Components/NoMatch/NoMatch';
+import PrivateRoute from './Components/Pages/Login/PrivateRoute/PrivateRoute';
+
+
+
+export const userContext = createContext();
+
 
 function App() {
+  const serviceData = [
+    {
+      icon: service1,
+      title: 'Web & Mobile design',
+      details: 'We craft stunning and amazing web UI, using a well drrafted UX to fit your product.'
+    },
+    {
+      icon: service2,
+      title: 'Graphic design',
+      details: 'Amazing flyers, social media posts and brand representations that would make your brand stand out.'
+    },
+    {
+      icon: service3,
+      title: 'Web development',
+      details: 'With well written codes, we build amazing apps for all platforms, mobile and web apps in general.'
+    }
+  ];
+  const [loggedInUser, loggedOutUser] = useState([]);
+  const [adminEmail, setAdminEmail] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/adminEmail')
+      .then(res => res.json())
+      .then(data => setAdminEmail(data))
+  }, [])
+
+  const admin = adminEmail.map(e => e.email);
+  // console.log(admin[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <userContext.Provider value={{ value1: [loggedInUser, loggedOutUser], value2: serviceData, value3: admin }}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <PrivateRoute path="/order">
+            <Order />
+          </PrivateRoute>
+          <Route path="/review">
+            <Review />
+          </Route>
+          <Route path="/serviceList">
+            <ServiceList />
+          </Route>
+          <Route path="/review">
+            <Review />
+          </Route>
+          <Route path="/adminServiceList">
+            <AdminServiceList />
+          </Route>
+          <Route path="/admin">
+            <AdminHome />
+          </Route>
+          <Route path="/addedService">
+            <Admin />
+          </Route>
+          <Route path="/makeAdmin">
+            <MakeAdmin />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </userContext.Provider >
   );
 }
 
---
+++ src/Components/Pages/Login/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { userContext } from '../../../../App';
+
+const PrivateRoute = ({children, ...rest}) => {
+    const { value1 } = useContext(userContext);
+    const [loggedInUser, setLoggedInUser] = value1;
+    return (
+        <div>
+             <Route
+                {...rest}
+                render={({ location }) =>
+                    loggedInUser.email ? (
+                        children
+                    ) : (
+                            <Redirect
+                                to={{
+                                    pathname: "/login",
+                                    state: { from: location }
+                                }}
+                            />
+                        )
+                }
+            />
+        </div>
+    );
+};
+
+export default PrivateRoute;
